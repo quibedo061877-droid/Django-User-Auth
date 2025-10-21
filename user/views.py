c67dc75e69d6d0ba973bb7af00ad7440b87dc016
Nonicks Quibedo <nonicksquibedo@gmail.com>
2025-10-21 11:54:58 +0800

    User Registration

--- user/views.py
+++ user/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse    
+from django.shortcuts import render, redirect
+from django.http import HttpResponse 
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout 
+
 
 # Create your views here.
 def home(request):
@@ -10,3 +13,8 @@ def register_view(request):
         username = request.POST['username']
         password = request.POST['password']
         confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            user = User.objects.create_user(username=username, password=password)
+            user.save()
+            return HttpResponse("Registration successful!")
