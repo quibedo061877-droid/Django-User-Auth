77f04236719908839d7e87af889773e2ac87b945
Nonicks Quibedo <nonicksquibedo@gmail.com>
2025-10-21 14:29:57 +0800

    Login Required

--- accounts/urls.py
+++ accounts/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('user.urls')),
-    path('user/', include('django.contrib.auth.urls'))
+    path('accounts/', include('user.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
--- user/views.py
+++ user/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse 
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout 
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
-
+@login_required
 # Create your views here.
 def home(request):
      return render(request, 'home.html')
