f0082db70c6262da5a65f2d1413387dea5b01bc9
Nonicks Quibedo <nonicksquibedo@gmail.com>
2025-10-21 12:02:31 +0800

    User Registration Templates

--- user/views.py
+++ user/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse 
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout 
+from django.contrib import messages
 
 
 # Create your views here.
@@ -15,6 +16,13 @@ def register_view(request):
         confirm_password = request.POST['confirm_password']
 
         if password == confirm_password:
-            user = User.objects.create_user(username=username, password=password)
-            user.save()
-            return HttpResponse("Registration successful!")
+            try:
+                user = User.objects.create_user(username=username, password=password)
+                user.save()
+                # return HttpResponse("Registration successful!")
+                return redirect('login')
+            except:
+                messages.error(request, "Username already exists.")
+        else:
+            messages.error(request, "Passwords do not match.")
+    return render(request, 'register.html')
